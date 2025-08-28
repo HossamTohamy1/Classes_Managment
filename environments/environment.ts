3a99fc96d799903e10e2fc67a20cf13a5ecfbece
HossamTohamy1 <tohamytohamy32@gmail.com>
2025-08-28 23:21:36 +0300

    Modified angular.json and environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7089', // تم تعديل البورت ليوافق السيرفر المحلي
+  apiUrl: 'https://c13aa1cd1956.ngrok-free.app',
+  // apiUrl: 'https://localhost:7089', // تم تعديل البورت ليوافق السيرفر المحلي
   apiTimeout: 30000
 };
